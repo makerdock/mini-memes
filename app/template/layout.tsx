22bbdda1891c9ddf078212526c70c0b2f2206332
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2025-05-26 04:32:09 +0530

    refactor(layout): enhance layout spacing for improved visual consistency
    - Updated the layout component to include space between elements, enhancing the overall visual structure.
    - Adjusted the flex properties to maintain a clean and organized appearance for the main content area.

--- app/template/layout.tsx
+++ app/template/layout.tsx
@@ -5,7 +5,7 @@ export default function TemplateLayout({ children }: { children: React.ReactNode
     return (
         <main className="min-h-screen bg-gradient-to-b from-purple-600 via-blue-500 to-pink-500 text-white">
             <div className="container mx-auto px-4 ">
-                <div className='min-h-screen flex flex-col justify-between'>
+                <div className='min-h-screen flex space-y-4 flex-col justify-between'>
                     <Header />
                     {children}
                     <Footer />
